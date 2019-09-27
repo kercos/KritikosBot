fa68a45b8f4e97f156cd2c57a257e83762fb2c95
Federico Sangati <federico.sangati@gmail.com>
2019-09-27 13:04:13 +0200

    change expired games every 4 hours (no every min)

--- messages.py
+++ messages.py
@@ -2,6 +2,6 @@
 
 import icons
 
-INSTRUCTIONS = icons.UNDER_CONSTRUCTION + " Instructions..."
+INSTRUCTIONS = "ChatBot to teach about fakenews. Contact @kercos for more info."
 
 NOT_VALID_INPUT = "{} Input not valid, please try again.".format(icons.EXCLAMATION_ICON)
